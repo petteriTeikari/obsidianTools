10f0da3b46ada57338a5ffbb87122123e044bebf
Petteri Teikari <petteri.teikari@gmail.com>
2024-06-23 00:30:37 +0300

    Notion export batch rename

--- remove_unused_figures/optimize_imgs_v2.py
+++ remove_unused_figures/optimize_imgs_v2.py
@@ -3,6 +3,8 @@ import glob
 import logging
 import os
 import re
+import time
+
 from PIL import Image
 
 from remove_unused_figures import get_disk_use, get_img_refs_from_md_files
@@ -27,19 +29,22 @@ def parse_args_to_dict():
     return args_dict
 
 
-def get_all_img_files(input_folder):
+def get_all_img_files(input_folder, folders=('Attachments', 'figures')):
 
     ext = ['*.png', '*.jpg', '*.jpeg', '*.gif', '*.svg']
     if isinstance(ext, list):
         files = []
         for e in ext:
-            files_e = glob.glob(os.path.join(input_folder, '**', e), recursive=True)
+            for folder in folders:
+                files_e = glob.glob(os.path.join(input_folder, folder, e), recursive=True)
+                files += files_e
+                # files_e = glob.glob(os.path.join(input_folder, '**', e), recursive=True)
             files += files_e
         files = sorted(files)
-        logging.info(f"Found {len(files)} (multiple extensions: {ext}) files from {input_folder}")
+        logging.info(f"Found {len(files)} (multiple extensions: {ext}) files from {input_folder}\n"
+                     f"\tvalid folders: {folders}")
     else:
-        files = glob.glob(os.path.join(input_folder, '**', ext), recursive=True)
-        logging.info(f"Found {len(files)} {ext} files from {input_folder}")
+        raise NotImplementedError("Only list of extensions is supported.")
 
     # replace whitespace with %20 in the listing
     files_out = []
@@ -119,7 +124,7 @@ def rename_extension_of_refs(img_refs, lines, unique_hash):
     return lines_as_str
 
 
-def process_md_file(md_file, img_files, md_fname, img_refs, input_folder, unique_hash, export_on=True):
+def process_md_file(md_file, img_refs, unique_hash, export_on=True):
 
     with open(md_file, 'r') as f:
         lines = f.readlines()
@@ -132,49 +137,67 @@ def process_md_file(md_file, img_files, md_fname, img_refs, input_folder, unique
         f.writelines(lines_out)
 
 
-def process_md_files_for_optimization(input_folder, unique_hash=None):
+def process_md_files_for_optimization(input_folder, use_hash: bool = True):
 
-    img_files = get_all_img_files(input_folder)
     md_files = sorted(glob.glob(os.path.join(input_folder, '**', '*.md'), recursive=True))
     img_refs = get_img_refs_from_md_files(md_files, args["input_folder"])
 
     for i, md_file in enumerate(md_files):
-        md_fname = os.path.basename(md_file)
-        logging.info(f"Processing {i+1}/{len(md_files)}: {md_fname}")
-        fname = os.path.basename(md_file)
+
+        if use_hash:
+            unique_hash = create_hash()
+        else:
+            unique_hash = None
+
+        base_dir, fname = os.path.split(md_file)
         fname, ext = os.path.splitext(fname)
+        logging.info(f"Processing {i + 1}/{len(md_files)}: {fname}")
         im_refs_file = img_refs[fname.replace(' ', '_')]
 
-        process_md_file(md_file, img_files, md_fname, im_refs_file, input_folder, unique_hash)
+        process_md_file(md_file, img_refs, unique_hash)
+
+        # TODO! Notion-specific, update for something more general
+        img_files = get_all_img_files(input_folder=os.path.join(base_dir, fname),
+                                      folders=('Attachments', 'figures'))
+        batch_convert_images(img_files,
+                             unique_hash=unique_hash)
+
+        time.sleep(1)
 
 
-def batch_convert_images(input_folder, quality=85, subsampling=0, unique_hash=None):
+def batch_convert_images(img_files, quality=85, subsampling=0, unique_hash=None):
 
     hash_string = hash_suffix_str(unique_hash)
 
     def correct_format(ext):
         return ext == '.png' or ext == '.jpeg'
 
-    img_files = get_all_img_files(input_folder)
     for i, img_file in enumerate(img_files):
         fname = os.path.basename(img_file)
         fname, ext = os.path.splitext(fname)
+
         if correct_format(ext):
-            logging.info(f"#{i+1}/{len(img_files)}: Converting {fname} to jpg")
+
             img_file = img_file.replace('%20', ' ')
             img = Image.open(img_file)
             img = img.convert('RGB')
             img_file_out = img_file.replace('.png', '.jpg').replace('.jpeg', '.jpg').replace('.gif', '.jpg')
             img_file_out = img_file_out.replace('.jpg', f'{hash_string}.jpg')
+
+            logging.info(f"#{i + 1}/{len(img_files)}: Converting {fname} to {os.path.basename(img_file_out)}")
             img.save(img_file_out, format='JPEG', subsampling=subsampling, quality=quality)
             os.rename(img_file, img_file + '.bakIMG')
+
         elif ext == '.jpg':
+
             if len(hash_string) > 0:
                 logging.info(f"#{i}/{len(img_files)}: Renaming {fname} to {fname}{hash_string}.jpg")
                 img_file = img_file.replace('%20', ' ')
                 img_file_out = img_file.replace('.jpg', f'{hash_string}.jpg')
-                os.rename(img_file, img_file_out)
-
+                try:
+                    os.rename(img_file, img_file_out)
+                except Exception as e:
+                    logging.error(f"Error: {e}")
 
 
 if __name__ == "__main__":
@@ -182,16 +205,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     args = parse_args_to_dict()
 
-    # TODO! Put this inside the .md file loop as you would like each folder to have
-    #  an unique hash for the images
     use_hash = True
-    if use_hash:
-        unique_hash = create_hash()
-    else:
-        unique_hash = None
 
     process_md_files_for_optimization(input_folder = args["input_folder"],
-                                      unique_hash = unique_hash)
-
-    batch_convert_images(input_folder = args["input_folder"],
-                         unique_hash = unique_hash)
+                                      use_hash = use_hash)
